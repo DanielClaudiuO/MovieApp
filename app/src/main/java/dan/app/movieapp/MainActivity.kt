db186df17a0db19afae732a026762073393570e6
DanielClaudiuO <daniel.oanta@gmail.com>
2022-07-19 12:56:37 +0300

    Initial Commit

---
+++ app/src/main/java/dan/app/movieapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package dan.app.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
